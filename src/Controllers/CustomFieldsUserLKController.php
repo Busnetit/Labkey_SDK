9ea2adb95aa87fed6ca8bf678e8b07ff762604a0
busnet <ioana@busnet.it>
2024-07-12 12:02:23 +0200

    custom fields

---
+++ src/Controllers/CustomFieldsUserLKController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Labkey\App\Controllers;
+
+use Illuminate\Support\Facades\Http;
+
+class CustomFieldsUserLKController extends AuthorizeLKController
+{
+
+    public function create(int $user_id, int $id_field, string $value, int $can_disable_user = null)
+    {
+        $response=Http::withToken($this->getToken())->post($this->url.'customfieldsuser/'.$user_id.'/'.$id_field.'/create', ["value"=>$value, "can_disable_user" => $can_disable_user]);
+        $this->badRequest($response);
+        if($response->json('status') != 'OK'){
+            return $response->json('message');
+        }
+        return true;
+    }
+
+    public function update(int $user_id,int $id_field,string $value, int $can_disable_user = null)
+    {
+        $response=Http::withToken($this->getToken())->post($this->url.'customfieldsuser/'.$user_id.'/'.$id_field.'/update', ["value"=>$value, "can_disable_user" => $can_disable_user]);
+        $this->badRequest($response);
+        if($response->json('status') != 'OK'){
+            return $response->json('message');
+        }
+        return true;
+    }
+
+    public function getAllByIdUser(int $user_id, int $limit = null,int $offset = null)
+    {
+       $response = Http::withToken($this->getToken())->get($this->url."customfieldsuser/".$user_id, ["limit" => $limit,"offset" => $offset] );
+        if ($response->status() !== 404) {
+            $this->badRequest($response);
+        }
+        if ($response->json('status') != 'KO') {
+            return $response->json('message');
+        }
+        return [];
+    }
+
+    public function getFieldUser(int $user_id, int $id_field)
+    {
+        $response = Http::withToken($this->getToken())->get($this->url . 'customfieldsuser/' . $user_id . "/" . $id_field);
+        if ($response->status() !== 404) {
+            $this->badRequest($response);
+        }
+        if ($response->json('status') != 'KO') {
+            return $response->json('message');
+        }
+        return [];
+    }
+}
--- src/Controllers/UserLKController.php
+++ src/Controllers/UserLKController.php
@@ -151,7 +151,6 @@ class UserLKController extends AuthorizeLKController{
     }
 
     public function doGrantAccess(int $user_id, string $data, int $key_id):array{
-        // dd(get_defined_vars());
         $response=Http::withToken($this->getToken())->post($this->url.'grantaccess',get_defined_vars());
         $this->badRequest($response);
         if($response->json('status') != 'OK'){
