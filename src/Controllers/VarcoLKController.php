99bdf95fadf9cf03cf8938f73f964800149f7934
busnet <ioana@busnet.it>
2024-07-17 17:01:30 +0200

    aggiunto get user varco detail

--- src/Controllers/VarcoLKController.php
+++ src/Controllers/VarcoLKController.php
@@ -15,7 +15,7 @@ class VarcoLKController extends AuthorizeLKController
         return [];
     }
     public function dropAccess($involved_associations){
-        $response = Http::withToken($this->getToken())->delete($this->url."dropaccess",get_defined_vars());
+        $response = Http::withToken($this->getToken())->withUserAgent('SDK-Labkey')->delete($this->url."dropaccess",get_defined_vars());
         $this->badRequest($response);
         if($response->json('status') != 'KO'){
             return $response->json('message');
@@ -24,14 +24,23 @@ class VarcoLKController extends AuthorizeLKController
     }
     public function isAccessible(string $unique_name,int $from_date,int $to_date,string $id_rele)
     {
-        $response=Http::withToken($this->getToken())->post($this->url.'isAccessible',get_defined_vars());
+        $response=Http::withToken($this->getToken())->withUserAgent('SDK-Labkey')->post($this->url.'isAccessible',get_defined_vars());
         $this->badRequest($response);
         return $response->json('message');
        
     }
 
     public function editaccess(int $user_id,int $key_id,string $data){
-        $response=Http::withToken($this->getToken())->post($this->url.'grantaccess',get_defined_vars());
+        $response=Http::withToken($this->getToken())->withUserAgent('SDK-Labkey')->post($this->url.'grantaccess',get_defined_vars());
+        $this->badRequest($response);
+        if($response->json('status') != 'KO'){
+            return $response->json('message');
+        }
+        return [];
+    }
+
+    public function getUserVarcoDetails(int $user_id){
+        $response = Http::withToken($this->getToken())->withUserAgent('SDK-Labkey')->get($this->url.'getuservarcodetails',get_defined_vars());
         $this->badRequest($response);
         if($response->json('status') != 'KO'){
             return $response->json('message');
